797551f51739f98526a9e0975b72fcdb117191b4
Hellend08 <sccp.hvasquez@gmail.com>
2019-07-02 18:52:34 -0500

    'funcion descargar cv'

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,9 +2,9 @@ const buttonDemo = document.getElementById('demo');
 const buttonRepo = document.getElementById('repo');
 
 buttonDemo.addEventListener('click', () => {
-  window.location = 'http://hellend08.github.io/LIM009-data-lovers/src/';
+  window.open('http://hellend08.github.io/LIM009-data-lovers/src/');
 });
 
 buttonRepo.addEventListener('click', () => {
-  window.location = 'https://github.com/hellend08/LIM009-data-lovers';
+  window.open('https://github.com/hellend08/LIM009-data-lovers');
 });
