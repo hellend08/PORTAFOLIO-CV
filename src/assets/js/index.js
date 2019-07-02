aee4f23765f04533e88f452db577321cf85cda61
Hellend08 <sccp.hvasquez@gmail.com>
2019-07-01 19:02:24 -0500

    'habilidades'

---
+++ src/assets/js/index.js
@@ -0,0 +1,10 @@
+const buttonDemo = document.getElementById('demo');
+const buttonRepo = document.getElementById('repo');
+
+buttonDemo.addEventListener('click', () => {
+  window.location = 'http://hellend08.github.io/LIM009-data-lovers/src/';
+});
+
+buttonRepo.addEventListener('click', () => {
+  window.location = 'https://github.com/hellend08/LIM009-data-lovers';
+});
