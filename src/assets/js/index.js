f6846e42244ecae2ae37b487a3242609efb78e74
Hellend08 <sccp.hvasquez@gmail.com>
2019-07-05 18:56:21 -0500

    'portafolio al 70%'

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,43 @@
-const buttonDemo = document.getElementById('demo');
-const buttonRepo = document.getElementById('repo');
+const buttonDemoDatalovers = document.getElementById('demo-datalovers');
+const buttonRepoDatalovers = document.getElementById('repo-datalovers');
 
-buttonDemo.addEventListener('click', () => {
+buttonDemoDatalovers.addEventListener('click', () => {
   window.open('http://hellend08.github.io/LIM009-data-lovers/src/');
 });
 
-buttonRepo.addEventListener('click', () => {
+buttonRepoDatalovers.addEventListener('click', () => {
   window.open('https://github.com/hellend08/LIM009-data-lovers');
+});
+
+const buttonDemoRedSocial = document.getElementById('demo-redsocial');
+const buttonRepoRedSocial = document.getElementById('repo-redsocial');
+
+buttonDemoRedSocial.addEventListener('click', () => {
+  window.open('https://hellend08.github.io/MINI-RS/src/index.html');
+});
+
+buttonRepoRedSocial.addEventListener('click', () => {
+  window.open('https://github.com/hellend08/MINI-RS');
+});
+
+const buttonDemoSecondChance = document.getElementById('demo-second-chance');
+const buttonRepoSecondChance = document.getElementById('repo-second-chance');
+
+buttonDemoSecondChance.addEventListener('click', () => {
+  window.open('https://hellend08.github.io/SECOND-CHANGE/');
+});
+
+buttonRepoSecondChance.addEventListener('click', () => {
+  window.open('https://github.com/hellend08/SECOND-CHANGE');
+});
+
+const buttonDemoMoviesLife = document.getElementById('demo-movies-life');
+const buttonRepoMoviesLife = document.getElementById('repo-movies-life');
+
+buttonDemoMoviesLife.addEventListener('click', () => {
+  window.open('');
+});
+
+buttonRepoMoviesLife.addEventListener('click', () => {
+  window.open('https://github.com/hellend08/LIM009-hackathon-peliculas');
 });
